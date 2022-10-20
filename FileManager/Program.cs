c92062542aa438738025b8897fcc4e8d2e809c38
Ivan <profftols@yandex.ru>
2022-10-20 09:36:08 +0500

    Add project files.

---
+++ FileManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
