78f1a5a8502fd44545d3952b63745d1ef03acafb
Ivan <profftols@yandex.ru>
2022-10-25 17:13:38 +0500

    Реализовываю меню выбора перехода в папки и файлы каталога. (без проверки на исключения, она будет позже)

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -12,8 +12,8 @@ namespace FileManager
     internal class Program
     {
         private static int num = 1;
+        private static int page;
         private static string disc;
-        private static string pathfile;
         private static string pathfolder;
         static void Form() //Поля для ограничения разделов
         {
@@ -31,27 +31,60 @@ namespace FileManager
                 PrintDriverDisc(0);
             }
 
-            PrintDriverDisc(UserCh()-1);
-            PrintFolder(pathfolder);
+            PrintDriverDisc(UserCh());
+            PrintFolder(pathfolder, 1);
+
+            do
+            {
+                string command = Console.ReadLine();
+                string[] com = command.Split(' ');
+                switch (com[0])
+                {
+                    case "ls":
+                        page = int.Parse(com[3]);
+                        PrintFolder(com[1], page);
+                        break;
+
+                    case "cp":
+
+                        break;
+
+                    case "rm":
+
+                        break;
+
+                    case "file":
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели неверное значение!");
+                        break;
+                }
+            } while (true);
         }
 
         static int UserCh()
         {
             Form();
             int i = int.Parse(Console.ReadLine());
-            return i;
+            return i - 1;
         }
 
-        static void PrintFolder(string fol)
+        static void PrintFolder(string fol, int npage)
         {
-            Console.WriteLine("Папки:");
             string[] folders = Directory.GetDirectories(fol);
+            int page = folders.Length % 7;
+
+
+
+            Console.WriteLine("Папки:");
             for (int i = 0; i < folders.Length; i++)
             {
                 Console.WriteLine(folders[i]);
             }
 
-            Console.WriteLine("Файлы:");
+            Console.WriteLine($"Файлы в папке {fol}:");
             string[] files = Directory.GetFiles(fol);
             foreach (var item in files)
             {
@@ -60,9 +93,9 @@ namespace FileManager
             Form();
         }
 
-        static void Inform (string ask) // Печать информации в папках и файлах
+        static void Inform(string ask) // Печать информации в папках и файлах
         {
-            
+
             Form();
         }
 
@@ -85,7 +118,7 @@ namespace FileManager
                 }
                 Form();
             }
-            Console.WriteLine("Приветствую в программе: Файловый менеджер! \n Тут будет отобраться файлы и их информация");
+            Console.WriteLine("Приветствую в программе: Файловый менеджер! \nтут будет отобраться файлы и их информация");
         }
     }
 }
